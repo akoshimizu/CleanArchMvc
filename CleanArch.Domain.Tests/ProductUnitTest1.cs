b7c4d41cac9c5c947a16c58779abe2a73b323208
akoshimizu <anderson_koshimizu@hotmail.com>
2022-12-26 13:58:34 -0300

    Add project files.

---
+++ CleanArch.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArch.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_RetultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .NotThrow<CleanArch.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-11, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+    }
+}
---
+++ CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,48 @@
+﻿using CleanArch.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+
+        public ICollection<Product> Products { get; set; }
+
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, 
+                "Invalid name, too short, minimum 3 charecters");
+
+            Name = name;
+        }
+    }
+}
