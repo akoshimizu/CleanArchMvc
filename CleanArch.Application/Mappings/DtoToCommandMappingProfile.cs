d5cee574799049717f3a09439c78dd26317d1e43
akoshimizu <anderson_koshimizu@hotmail.com>
2022-12-27 09:29:33 -0300

    Inclusão - CQRS

---
+++ CleanArch.Application/Mappings/DtoToCommandMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿
+using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Products.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Mappings
+{
+    public class DtoToCommandMappingProfile : Profile
+    {
+        public DtoToCommandMappingProfile()
+        {
+            CreateMap<ProductDTO, ProductCreateCommand>();
+            CreateMap<ProductDTO, ProductUpdateCommand>();
+        }
+    }
+}
